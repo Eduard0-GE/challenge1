2bf93903c15313ff3990bb7700e17f6267df427e
gerson <gersonabes@gmail.com>
2023-08-21 14:36:34 -0300

    feat created the method to create operations 'createOperations' . I also fixed some indentations

--- Q-03/src/Java/Calculator.java
+++ Q-03/src/Java/Calculator.java
@@ -4,18 +4,27 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-        public int operandAmt(){
-            int operandAmount = 0;
-            Scanner input = new Scanner(System.in);
-            while(true){
-                try{
-                    operandAmount = input.nextInt();
-                    break;
-                }catch(InputMismatchException e){
-                    System.out.println("Invalid input type!: Try again!");
-                    input.next();
-                }
+    public int operandAmt() {
+        int operandAmount = 0;
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            try {
+                operandAmount = input.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input type!: Try again!");
+                input.next();
             }
+        }
         return operandAmount;
+    }
+
+    public String createOperation(int operandAmount) {
+        Scanner input = new Scanner(System.in);
+        String operation;
+
+        operation = input.next();
+
+        return operation;
     }
 }
