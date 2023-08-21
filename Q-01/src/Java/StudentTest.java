7dcd3f39ccba47e4f2ed37c010497fc0ca6c4884
gerson <gersonabes@gmail.com>
2023-08-21 14:15:33 -0300

    feat: created the createStudents method that will add students to the StudentTest list

--- Q-01/src/Java/StudentTest.java
+++ Q-01/src/Java/StudentTest.java
@@ -38,4 +38,32 @@ public class StudentTest {
         }
         return qtdAlunos;
     }
+    
+    public void createStudents(Scanner scanner, int studentAmount) {
+
+        int counter = 0;
+
+        while (counter < studentAmount) {
+            String name = "";
+            int qtdSolved = 0;
+            System.out.print("Enter the student's name and amount of questions solved: ");
+            try {
+                name = scanner.next();
+                qtdSolved = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input type! Try again!");
+                scanner.next();
+                continue;
+            }
+            if (name.length() > 20) {
+                System.out.print("Name excedeed the maximum lenght");
+                continue;
+            } else if (qtdSolved < 0 || qtdSolved > 10) {
+                System.out.println("Solved questions out of range(min: 0 - max: 10)");
+                continue;
+            }
+            studentList.add(new Students(name, qtdSolved));
+            counter++;
+        }
+    }
 }
