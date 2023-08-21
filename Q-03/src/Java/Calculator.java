ea6a195a970fd6a3f0ac7ebc92ce7fc4175304f2
gerson <gersonabes@gmail.com>
2023-08-21 14:37:26 -0300

    feat: created the operationResult method that prints the result of the operations

--- Q-03/src/Java/Calculator.java
+++ Q-03/src/Java/Calculator.java
@@ -1,6 +1,8 @@
 package Java;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
@@ -26,5 +28,31 @@ public class Calculator {
         operation = input.next();
 
         return operation;
+    }
+
+    public void operationResult(String operation, int operandAmount){
+        List<String> operator = new ArrayList<>();
+        List<Integer> operand = new ArrayList<>();
+        int lastIndex = 0;
+
+        for(int i = 0; i < operation.length(); i++){
+            if(operation.charAt(i) == '-' || operation.charAt(i) == '+'){
+                operator.add(String.valueOf( operation.charAt(i)));
+                operand.add(Integer.parseInt(operation.substring(lastIndex, i)));
+                lastIndex = i + 1;
+            }
+        }
+        operand.add(Integer.parseInt(operation.substring(lastIndex, operation.length())));
+
+        int result = operand.get(0);
+
+        for(int i = 0; i < operandAmount - 1; i++){
+            if(operator.get(i).equals("+")){
+                result = result + operand.get(i + 1);
+            }else{
+                result = result - operand.get(i + 1);
+            }
+        }
+        System.out.printf("%d\n",result);
     }
 }
