8a1b0827c1757a8c84a5496a9ce5176d70a15a7c
gerson <gersonabes@gmail.com>
2023-08-21 14:26:05 -0300

    feat: created the EmotionCheck class with its getters and setters

---
+++ Q-02/src/Java/EmotionCheck.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class EmotionCheck {
+    private int fun;
+    private int upset;
+
+    public int getFun() {
+        return fun;
+    }
+
+    public void setFun(int fun) {
+        this.fun = fun;
+    }
+
+    public int getUpset() {
+        return upset;
+    }
+
+    public void setUpset(int upset) {
+        this.upset = upset;
+    }
+}
