ff8263ca09dc863c445719abcb9538ef8d5a4703
gerson <gersonabes@gmail.com>
2023-08-21 14:34:35 -0300

    feat: created the operandAmt method that returns the amount of operands in the equation

--- Q-03/src/Java/Calculator.java
+++ Q-03/src/Java/Calculator.java
@@ -1,5 +1,21 @@
 package Java;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculator {
-    
+        public int operandAmt(){
+            int operandAmount = 0;
+            Scanner input = new Scanner(System.in);
+            while(true){
+                try{
+                    operandAmount = input.nextInt();
+                    break;
+                }catch(InputMismatchException e){
+                    System.out.println("Invalid input type!: Try again!");
+                    input.next();
+                }
+            }
+        return operandAmount;
+    }
 }
