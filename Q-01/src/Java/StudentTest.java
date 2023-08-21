88084942d67ffa7bd730b374677052972480cbe7
gerson <gersonabes@gmail.com>
2023-08-21 14:14:06 -0300

    feat: created the setStudentAmount method that will return the amount of students on the competition

--- Q-01/src/Java/StudentTest.java
+++ Q-01/src/Java/StudentTest.java
@@ -13,4 +13,29 @@ public class StudentTest {
     public HashMap<Integer, ArrayList<String>> getStudentHashMap() {
         return studentByAmountSolved;
     }
+    public int setStudentAmount(Scanner scanner) {
+        boolean qtdValida = false;
+        int qtdAlunos = 0;
+
+        while (!qtdValida) {
+            System.out.print("Enter the number of students of the class (min: 1, max: 100, exit:0): ");
+            try {
+                qtdAlunos = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input type! Try again using an integer!");
+                scanner.next();
+                continue;
+            }
+            if (qtdAlunos > 0 && qtdAlunos <= 100) {
+                System.out.println("Number of students: " + qtdAlunos);
+                qtdValida = true;
+            } else if (qtdAlunos == 0) {
+                qtdValida = true;
+            } else {
+                System.out.println("Invalid student amount! Try again!");
+                scanner.next();
+            }
+        }
+        return qtdAlunos;
+    }
 }
